7c861344527bfbf19e437ba30831df24fe88d95d
mmoebes10 <maleea.grace@gmail.com>
2024-07-24 15:41:04 -0400

    booya

--- tools/fw_protect.py
+++ tools/fw_protect.py
@@ -9,15 +9,33 @@ Firmware Bundle-and-Protect Tool
 """
 import argparse
 from pwn import *
-
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from base64 import b64encode
 
 def protect_firmware(infile, outfile, version, message):
     # Load firmware binary from infile
     with open(infile, "rb") as fp:
-        firmware = fp.read()
+       firmware = pad(fp.read(), 250)
+
+    # Write code here
+    header = b'\x00\x00\x00'
+    key = open('secret_key.txt', 'rb').read()
+
+    protected_firmware = ''
+
+    for chunk in range(0, len(firmware), 250):
+        chunk = firmware[chunk:chunk+250]
+        cipher = AES.new(key, AES.MODE_GCM)
+        cipher.update(header)
+        nonce = cipher.nonce
+
+
+        ciphertext, tag = cipher.encrypt_and_digest(header + chunk)
+        encrypted_message = b64encode(nonce + tag + ciphertext).decode('utf-8')
 
     # Append null-terminated message to end of firmware
-    firmware_and_message = firmware + message.encode() + b"\00"
+    firmware_and_message = protected_firmware.encode() + b"\00"
 
     # Pack version and size into two little-endian shorts
     metadata = p16(version, endian='little') + p16(len(firmware), endian='little')  
