50979a54faeff85d517cc64bc708db5ae40ad47e
ParkingSoman <senguptashonu@gmail.com>
2024-07-26 15:47:43 -0400

    removed iv since established in fw_protect.py

--- tools/bl_build.py
+++ tools/bl_build.py
@@ -20,7 +20,6 @@ BOOTLOADER_DIR = os.path.join(REPO_ROOT, "bootloader")
 
 # lengths for secrets
 AES_KEY_LEN = 32
-IV_LEN = 12
 
 def make_bootloader() -> bool:
 
@@ -30,10 +29,6 @@ def make_bootloader() -> bool:
         aes_key = get_random_bytes(AES_KEY_LEN)
         c_aes_key = ', '.join(f'0x{byte:02x}' for byte in aes_key)
 
-        # create iv and c array format
-        iv = get_random_bytes(IV_LEN)
-        c_iv = ', '.join(f'0x{byte:02x}' for byte in iv)
-
         # create secrets header for bootloader and write
         with open("/home/hacker/cohesion-embsec/bootloader/inc/secrets.h", "w") as secrets_header:
             # write dependencies
@@ -41,17 +36,12 @@ def make_bootloader() -> bool:
 
             # write lengths
             secrets_header.write(f'#define AES_KEY_LEN {AES_KEY_LEN}\n')
-            secrets_header.write(f'#define AES_IV_LEN {IV_LEN}\n')
 
             # write aes key within "aes_key" section of compiled binary
             secrets_header.write(f'uint8_t AES_KEY[] = {{{c_aes_key}}};\n')
 
-            # write iv
-            secrets_header.write(f'const uint8_t AES_IV[] = {{{c_iv}}};\n')
-
         # write to secrets text file
         secrets_txt.write(aes_key)
-        secrets_txt.write(iv)
 
     # Build the bootloader from source.
 
